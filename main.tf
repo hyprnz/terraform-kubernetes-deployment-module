f9ec76955c1d75f76cd48b310d7f36aca9f2e73a
Reuben Dunn <reuben.dunn@hypr.co.nz>
2019-07-24 22:37:13 +1200

    Updated service

--- main.tf
+++ main.tf
@@ -17,3 +17,286 @@ resource "kubernetes_secret" "this" {
   }
   type = "Opaque"
 }
+
+# apiVersion: v1
+# kind: ServiceAccount
+# metadata:
+#   labels:
+#     k8s-app: kubernetes-dashboard
+#   name: kubernetes-dashboard
+#   namespace: kube-system
+
+resource "kubernetes_service_account" "this" {
+  metadata {
+    name = "kubernetes-dashboard"
+    namespace = "kube-system"
+    labels = {
+      k8s-app = "kubernetes-dashboard"
+    }
+  }
+}
+
+# kind: Role
+# apiVersion: rbac.authorization.k8s.io/v1
+# metadata:
+#   name: kubernetes-dashboard-minimal
+#   namespace: kube-system
+# rules:
+#   # Allow Dashboard to create 'kubernetes-dashboard-key-holder' secret.
+# - apiGroups: [""]
+#   resources: ["secrets"]
+#   verbs: ["create"]
+#   # Allow Dashboard to create 'kubernetes-dashboard-settings' config map.
+# - apiGroups: [""]
+#   resources: ["configmaps"]
+#   verbs: ["create"]
+#   # Allow Dashboard to get, update and delete Dashboard exclusive secrets.
+# - apiGroups: [""]
+#   resources: ["secrets"]
+#   resourceNames: ["kubernetes-dashboard-key-holder", "kubernetes-dashboard-certs"]
+#   verbs: ["get", "update", "delete"]
+#   # Allow Dashboard to get and update 'kubernetes-dashboard-settings' config map.
+# - apiGroups: [""]
+#   resources: ["configmaps"]
+#   resourceNames: ["kubernetes-dashboard-settings"]
+#   verbs: ["get", "update"]
+#   # Allow Dashboard to get metrics from heapster.
+# - apiGroups: [""]
+#   resources: ["services"]
+#   resourceNames: ["heapster"]
+#   verbs: ["proxy"]
+# - apiGroups: [""]
+#   resources: ["services/proxy"]
+#   resourceNames: ["heapster", "http:heapster:", "https:heapster:"]
+#   verbs: ["get"]
+
+resource "kubernetes_role" "this" {
+  metadata {
+    name = "kubernetes-dashboard-minimal"
+    namespace = "kube-system"
+  }
+  rule {
+     # Allow Dashboard to create 'kubernetes-dashboard-key-holder' secret.
+    api_groups     = [""]
+    resources      = ["secrets"]
+    verbs          = ["create"]
+  }
+  rule = {
+    # Allow Dashboard to create 'kubernetes-dashboard-settings' config map.
+    api_groups = [""]
+    resources  = ["configmaps"]
+    verbs      = ["create"]
+  }
+  rule {
+    # Allow Dashboard to get, update and delete Dashboard exclusive secrets.
+    api_groups = [""]
+    resources  = ["configmaps"]
+    resource_names = ["kubernetes-dashboard-key-holder", "kubernetes-dashboard-certs"]
+    verbs      = ["get", "update", "delete"]
+  }
+  rule {
+    # Allow Dashboard to create 'kubernetes-dashboard-settings' config map.
+    api_groups = [""]
+    resources = ["configmaps"]
+    resource_names  = ["kubernetes-dashboard-settings"]
+    verbs      = ["create"]
+  }
+  rule {
+    # Allow Dashboard to get metrics from heapster.
+    api_groups = [""]
+    resources = ["services"]
+    resource_names  = ["heapster"]
+    verbs      = ["proxy"]
+  }
+  rule {
+    api_groups = [""]
+    resources = ["services/proxy"]
+    resource_names  = ["heapster", "http:heapster:", "https:heapster:"]
+    verbs      = ["get"]
+  }
+}
+
+# apiVersion: rbac.authorization.k8s.io/v1
+# kind: RoleBinding
+# metadata:
+#   name: kubernetes-dashboard-minimal
+#   namespace: kube-system
+# roleRef:
+#   apiGroup: rbac.authorization.k8s.io
+#   kind: Role
+#   name: kubernetes-dashboard-minimal
+# subjects:
+# - kind: ServiceAccount
+#   name: kubernetes-dashboard
+#   namespace: kube-system
+
+resource "kubernetes_role_binding" "this" {
+  metadata {
+    name      = "kubernetes-dashboard-minimal"
+    namespace = "kube-system"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = "kubernetes-dashboard-minimal"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "kubernetes-dashboard"
+    namespace = "kube-system"
+  }
+}
+
+# kind: Deployment
+# apiVersion: apps/v1
+# metadata:
+#   labels:
+#     k8s-app: kubernetes-dashboard
+#   name: kubernetes-dashboard
+#   namespace: kube-system
+# spec:
+#   replicas: 1
+#   revisionHistoryLimit: 10
+#   selector:
+#     matchLabels:
+#       k8s-app: kubernetes-dashboard
+#   template:
+#     metadata:
+#       labels:
+#         k8s-app: kubernetes-dashboard
+#     spec:
+#       containers:
+#       - name: kubernetes-dashboard
+#         image: k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.1
+#         ports:
+#         - containerPort: 8443
+#           protocol: TCP
+#         args:
+#           - --auto-generate-certificates
+#           # Uncomment the following line to manually specify Kubernetes API server Host
+#           # If not specified, Dashboard will attempt to auto discover the API server and connect
+#           # to it. Uncomment only if the default does not work.
+#           # - --apiserver-host=http://my-address:port
+#         volumeMounts:
+#         - name: kubernetes-dashboard-certs
+#           mountPath: /certs
+#           # Create on-disk volume to store exec logs
+#         - mountPath: /tmp
+#           name: tmp-volume
+#         livenessProbe:
+#           httpGet:
+#             scheme: HTTPS
+#             path: /
+#             port: 8443
+#           initialDelaySeconds: 30
+#           timeoutSeconds: 30
+#       volumes:
+#       - name: kubernetes-dashboard-certs
+#         secret:
+#           secretName: kubernetes-dashboard-certs
+#       - name: tmp-volume
+#         emptyDir: {}
+#       serviceAccountName: kubernetes-dashboard
+#       # Comment the following tolerations if Dashboard must not be deployed on master
+#       tolerations:
+#       - key: node-role.kubernetes.io/master
+#         effect: NoSchedule
+
+resource "kubernetes_deployment" "this" {
+  metadata {
+    name = "kubernetes-dashboard"
+    namespace = "kube-system"
+    labels = {
+      k8s-app = "kubernetes-dashboard"
+    }
+  }
+  spec {
+    replicas = 1
+    revision_history_limit = 10
+    selector {
+      match_labels = {
+        k8s-app = "kubernetes-dashboard"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          k8s-app = "kubernetes-dashboard"
+        }
+      }
+      spec {
+        container {
+          image = "k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.1"
+          name  = "kubernetes-dashboard"
+          port {
+            container_port = 8443
+            protocol = "TCP"
+          }
+          args = ["--auto-generate-certificates"]
+          volume_mount {
+            name = "kubernetes-dashboard-certs"
+            mount_path = "/certs"
+          }
+          volume_mount {
+            name = "tmp-volume"
+            mount_path = "/tmp"
+          }
+          liveness_probe {
+            http_get {
+              scheme = "HTTPS"
+              path = "/"
+              port = 8443
+            }
+            initial_delay_seconds = 30
+            timeout_seconds       = 30
+          }
+        }
+        volume {
+          name = "kubernetes-dashboard-certs"
+          secret {
+            secret_name = "kubernetes-dashboard-certs"
+          }
+        }
+        volume {
+          name = "tmp-volume"
+          empty_dir = [{}]
+        }
+        service_account_name = "kubernetes-dashboard"
+      }
+    }
+  }
+}
+
+# kind: Service
+# apiVersion: v1
+# metadata:
+#   labels:
+#     k8s-app: kubernetes-dashboard
+#   name: kubernetes-dashboard
+#   namespace: kube-system
+# spec:
+#   ports:
+#     - port: 443
+#       targetPort: 8443
+#   selector:
+#     k8s-app: kubernetes-dashboard
+
+resource "kubernetes_service" "this" {
+  metadata {
+    name = "kubernetes-dashboard"
+    namespace = "kube-system"
+    labels = {
+      k8s-app = "kubernetes-dashboard"
+    }
+  }
+  spec {
+    selector = {
+      k8s-app = "kubernetes-dashboard"
+    }
+    port {
+      port        = 443
+      target_port = 8443
+    }
+    type = "ClusterIP"
+  }
+}
