aff4788259f8075d444dde33efc2ca5113de799f
Reuben Dunn <reuben.dunn@hypr.co.nz>
2019-09-11 08:59:40 +1200

    Added module example with rds

--- example/rds_datastore/main.tf
+++ example/rds_datastore/main.tf
@@ -1,9 +1,12 @@
 module "example" {
   source = "../../"
 
-  eks_cluster_name  = "eks-uat-jarden"
+  eks_cluster_name  = "eks-stage-example"
   app_name          = "example-service"
-  namespace         = "kube-system"
 
-  
+  enable_datastore_module = true
+  enable_rds              = true
+  rds_dbname              = "example"
+  rds_identifier          = "example-stage-postgres"
+  rds_password            = "1234567890absDEFghiJKL"
 }
