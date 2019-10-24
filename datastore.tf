d1531b1ee8deec5b0103e6eb87e552c0489496df
Reuben Dunn <reuben.dunn@hypr.co.nz>
2019-10-24 14:15:31 +1300

    Updated datastore module reference to 0.1.6

--- datastore.tf
+++ datastore.tf
@@ -1,5 +1,5 @@
 module "service_datastore" {
-  source = "git::git@github.com:hyprnz/terraform-aws-data-storage-module?ref=0.1.5"
+  source = "git::git@github.com:hyprnz/terraform-aws-data-storage-module?ref=0.1.6"
 
   providers = {
     aws = "aws"
