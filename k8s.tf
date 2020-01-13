134c6e22903e119d1a49c08bc7ae7d1fb086cb3f
Reuben Dunn <reuben.dunn@hypr.co.nz>
2020-01-14 09:32:15 +1300

    Refactored K8s provider to own file

---
+++ k8s.tf
@@ -0,0 +1,15 @@
+data "aws_eks_cluster" "this" {
+  name = var.eks_cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.eks_cluster_name
+}
+
+#Provider version set in versions.tf
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
+}
--- versions.tf
+++ versions.tf
@@ -2,13 +2,6 @@ terraform {
   required_version = ">= 0.12"
   required_providers {
     aws = "~>2.19"
+    kubernetes = "~>1.8"
   }
-}
-
-provider "kubernetes" {
-  version                = "~>1.8"
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
 }
